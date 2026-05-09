ceeb276b673a83e82ca99e7dd0112f0ba7dc7855
Parthsathbhai <parthsatbhai3@gmail.com>
2026-05-09 18:55:20 +0530

    Create Searching, Sorting, Counting, Broadcasting.py

---
+++ Searching, Sorting, Counting, Broadcasting.py
@@ -0,0 +1,22 @@
+import numpy as np
+
+# Input array from the user
+array1 = np.array(list(map(int, input().split())))
+
+# Searching
+search_value = int(input("Value to search: "))
+count_value = int(input("Value to count: "))
+broadcast_value = int(input("Value to add: "))
+
+# Find indices where value matches in array1
+indices = np.where(array1 == search_value)[0]
+print(indices)
+# Count occurrences in array1
+count = np.count_nonzero(array1 == count_value)
+print(count)
+# Broadcasting addition
+broadcast_result = array1 + broadcast_value
+print(broadcast_result)
+# Sort the first array
+sorted_array = np.sort(array1)
+print(sorted_array)
